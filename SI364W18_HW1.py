8acdf5becdcd0c007f1494ead6fc27b0227df395
Amanda Gomez <amandagomez@Amandas-MacBook-Air-2.local>
2018-01-14 23:29:49 -0500

    HW1 completion

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -11,21 +11,27 @@
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, request
+import requests
+import json
+
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
+    return 'Welcome to SI364!'
 
 
-if __name__ == '__main__':
-    app.run()
+
 
 
 ## [PROBLEM 2] - 250 points
-## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', you should see something like the data shown in the included file sample_ratatouille_data.txt, which contains data about the animated movie Ratatouille. However, if you go to the url http://localhost:5000/movie/titanic, you should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl' for example, you should see data on the page that looks like this:
+## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' 
+#you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', 
+#you should see something like the data shown in the included file sample_ratatouille_data.txt, which contains data about the animated movie Ratatouille.
+# However, if you go to the url http://localhost:5000/movie/titanic, you should get different data, and if you go to the url 
+#'http://localhost:5000/movie/dsagdsgskfsl' for example, you should see data on the page that looks like this:
 
 # {
 #  "resultCount":0,
@@ -38,14 +44,30 @@ if __name__ == '__main__':
 ## Of course, you'll also need the requests library and knowledge of how to make a request to a REST API for data.
 
 ## Run the app locally (repeatedly) and try these URLs out!
-
+@app.route('/movie/<moviename>')
+def movies(moviename):
+	url= 'https://itunes.apple.com/search?term={}&country=US&entity=movie'.format(moviename)
+	r= requests.get(url)
+	lo=r.json()
+	return '{}'.format(lo)
+	
+	
 ## [PROBLEM 3] - 250 points
 
 ## Edit the above Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number.
 ## Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
-
-
+@app.route('/question')
+def quest():
+	formstring="""<br><br>
+    <form action="http://localhost:5000/result" method='POST'>
+	<input type="number" name="favorite number"><br> Enter your favorite number 
+	<input type="submit" value="Submit"></form>"""
+	return formstring
+@app.route('/result',methods=['POST'])
+def page():
+	return'<br>Double your favorite number is {}</br>'.format(int(request.form['favorite number'])*2)
+	
 ## [PROBLEM 4] - 350 points
 
 ## Come up with your own interactive data exchange that you want to see happen dynamically in the Flask application, and build it into the above code for a Flask application, following a few requirements.
@@ -65,3 +87,18 @@ if __name__ == '__main__':
 # You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
 
 # Points will be assigned for each specification in the problem.
+@app.route('/problem4form/<tvshow>')
+
+def form1():
+	url= 'https://itunes.apple.com/search?term={}&country=US&entity=tvShow'.format(tvshow)
+	formstring1= """ <form action="http://localhost:5000/results" method='GET'>
+  <input type="checkbox" name="show1" value="comedy"> Comedy<br>
+  <input type="checkbox" name="show2" value="drama"> Drama<br>
+  <input type="submit" value="Submit">
+</form>"""
+@app.route('/results', methods=['GET'])
+def form():
+	if 
+	return 
+if __name__ == '__main__':
+    app.run()
